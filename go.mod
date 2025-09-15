7704c9d6a81f69fe8385705317d258d51b14b7a7
Richard Hull <rm_hull@yahoo.co.uk>
2025-09-15 23:20:46 +0100

    Add cobra for command handling

--- go.mod
+++ go.mod
@@ -17,6 +17,7 @@ require (
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/golang/snappy v1.0.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/influxdata/influxdb-client-go/v2 v2.14.0 // indirect
 	github.com/influxdata/line-protocol v0.0.0-20210922203350-b1ad95c89adf // indirect
 	github.com/klauspost/compress v1.18.0 // indirect
@@ -29,6 +30,7 @@ require (
 	github.com/prometheus/procfs v0.17.0 // indirect
 	github.com/rabbitmq/amqp091-go v1.10.0 // indirect
 	github.com/redis/go-redis/v9 v9.14.0 // indirect
+	github.com/spf13/pflag v1.0.10 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
 	github.com/xdg-go/scram v1.1.2 // indirect
 	github.com/xdg-go/stringprep v1.0.4 // indirect
@@ -45,6 +47,7 @@ require (
 	github.com/bytedance/sonic/loader v0.3.0 // indirect
 	github.com/cloudwego/base64x v0.1.6 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.10 // indirect
+	github.com/gin-contrib/pprof v1.5.3
 	github.com/gin-contrib/sse v1.1.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
@@ -57,6 +60,7 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.4 // indirect
+	github.com/spf13/cobra v1.10.1
 	github.com/tavsec/gin-healthcheck v1.7.10
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.3.0 // indirect
---
+++ temp.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"regexp"
+	"strconv"
+
+	"github.com/joho/godotenv"
+	"github.com/rm-hull/metoffice-uk-weather-overlays/internal"
+	"github.com/rm-hull/metoffice-uk-weather-overlays/internal/png"
+)
+
+func TestFetch() {
+
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
+	}
+
+	apiKey := os.Getenv("METOFFICE_DATAHUB_API_KEY")
+	if apiKey == "" {
+		log.Fatal("Error: METOFFICE_DATAHUB_API_KEY environment variable not set.")
+	}
+
+	client := internal.NewDataHubClient(apiKey)
+	resp, err := client.GetLatest("o205748062845")
+	if err != nil {
+		panic(err)
+	}
+
+	re := regexp.MustCompile(`(.*?)_ts(\d{1,2})_(\d{4})(\d{2})(\d{2})00`)
+
+	for _, file := range resp.OrderDetails.Files {
+		matches := re.FindStringSubmatch(file.FileId)
+		if matches == nil {
+			continue
+		}
+
+		path, err := createPath(matches)
+		if err != nil {
+			panic(err)
+		}
+
+		hour, err := strconv.Atoi(matches[2])
+		if err != nil {
+			panic(err)
+		}
+
+		kind := matches[1]
+		filename := fmt.Sprintf("%s/%02d.png", path, hour)
+
+		if _, err := os.Stat(filename); err == nil {
+			// File already exists, skip.
+			continue
+		} else if !os.IsNotExist(err) {
+			// An unexpected error occurred (e.g., permissions).
+			panic(err)
+		}
+
+		inFile, err := client.GetLatestDataFile(resp.OrderDetails.Order.OrderId, file.FileId)
+		if err != nil {
+			panic(err)
+		}
+
+		outFile, err := os.Create(filename)
+		if err != nil {
+			panic(err)
+		}
+
+		if kind == "total_precipitation_rate" {
+			err = png.Smooth(inFile, outFile, 50, 1.0)
+		} else {
+			_, err = io.Copy(outFile, inFile)
+		}
+		if err != nil {
+			panic(err)
+		}
+		if err := inFile.Close(); err != nil {
+			log.Printf("failed to close data file: %v", err)
+		}
+		if err := outFile.Close(); err != nil {
+			log.Printf("failed to close data file: %v", err)
+		}
+	}
+}
+
+func CreateAnimation() {
+
+	dirPath := "data/datahub/temperature_at_surface/2025/09/15/"
+	entries, err := os.ReadDir(dirPath)
+	if err != nil {
+		panic(err)
+	}
+
+	files := make([]string, len(entries))
+	for i, entry := range entries {
+		fmt.Println(entry.Name()) // just the filename
+		files[i] = dirPath + entry.Name()
+	}
+
+	apngBytes, err := png.Animate(files, 1.0)
+	if err != nil {
+		panic(err)
+	}
+
+	err = os.WriteFile("data/temp.png", apngBytes, 0644)
+	if err != nil {
+		panic(err)
+	}
+
+}
+
+func createPath(matches []string) (string, error) {
+	path := fmt.Sprintf("data/datahub/%s/%s/%s/%s", matches[1], // type
+		matches[3], // year
+		matches[4], // month
+		matches[5], // day
+	)
+	if err := os.MkdirAll(path, 0755); err != nil {
+		return "", err
+	}
+	return path, nil
+}
