a9191d4667fe511027df68f1d52b137819e543b8
Richard Hull <rm_hull@yahoo.co.uk>
2025-09-16 13:51:53 +0100

    Add scheduler (#5)
    * Add scheduler
    
    * Address PR comments

--- cmd/api_server.go
+++ cmd/api_server.go
@@ -4,16 +4,34 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/Depado/ginprom"
 	"github.com/gin-contrib/pprof"
 	"github.com/gin-gonic/gin"
+	"github.com/rm-hull/metoffice-uk-weather-overlays/internal"
 	healthcheck "github.com/tavsec/gin-healthcheck"
 	"github.com/tavsec/gin-healthcheck/checks"
 	hc_config "github.com/tavsec/gin-healthcheck/config"
 )
 
 func ApiServer(rootDir string, port int, debug bool) {
+
+	apiKey := os.Getenv("METOFFICE_DATAHUB_API_KEY")
+	if apiKey == "" {
+		log.Fatal("Error: METOFFICE_DATAHUB_API_KEY environment variable not set.")
+	}
+
+	orderId := os.Getenv("METOFFICE_ORDER_ID")
+	if orderId == "" {
+		log.Fatal("Error: METOFFICE_ORDER_ID environment variable not set.")
+	}
+
+	sched, err := internal.NewScheduler(apiKey, orderId, rootDir)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	r := gin.New()
 
 	prometheus := ginprom.New(
@@ -33,7 +51,7 @@ func ApiServer(rootDir string, port int, debug bool) {
 		pprof.Register(r)
 	}
 
-	err := healthcheck.New(r, hc_config.DefaultConfig(), []checks.Check{})
+	err = healthcheck.New(r, hc_config.DefaultConfig(), []checks.Check{})
 	if err != nil {
 		log.Fatalf("failed to initialize healthcheck: %v", err)
 	}
@@ -44,5 +62,10 @@ func ApiServer(rootDir string, port int, debug bool) {
 	log.Printf("Starting HTTP API Server on port %d...", port)
 	if err := r.Run(addr); err != nil && err != http.ErrServerClosed {
 		log.Fatalf("HTTP API Server failed to start on port %d: %v", port, err)
+	}
+
+	err = sched.Shutdown()
+	if err != nil {
+		log.Fatalf("failed to shutdown scheduler: %v", err)
 	}
 }
